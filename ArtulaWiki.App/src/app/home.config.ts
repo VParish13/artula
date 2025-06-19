9f485cef8cebe1e8229dd489632c0bce1b5c9e85
Vinny Parish <VParish13@gmail.com>
2025-06-19 16:49:54 -0500

    Additional organization and such

---
+++ ArtulaWiki.App/src/app/home.config.ts
@@ -0,0 +1,12 @@
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './home.routes';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZonelessChangeDetection(),
+    provideRouter(routes)
+  ]
+};
